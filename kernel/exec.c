9b92f92ac2a186d365d20f3dafaa7f30b3d78c57
szy <2152592@tongji.edu.cn>
2023-08-11 17:09:22 +0800

    811

--- kernel/exec.c
+++ kernel/exec.c
@@ -75,6 +75,9 @@ exec(char *path, char **argv)
   sp = sz;
   stackbase = sp - PGSIZE;
 
+  // 添加复制逻辑
+  u2kvmcopy(pagetable,p->kernelpt,0,sz);
+
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
     if(argc >= MAXARG)
@@ -116,10 +119,8 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
-  //进程的pid=1代表是第一个进程
-  if (p->pid == 1) {
+  if(p->pid==1)
     vmprint(p->pagetable);
-  }
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
